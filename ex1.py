1fab663f3013a15e337f6c1743bb7b820263b15f
Suba-Varshini <subavarshini7@gmail.com>
2023-07-09 15:33:33 +0530

    ex1.py

---
+++ ex1.py
@@ -0,0 +1,11 @@
+num = 725
+salary = 2345.78
+var = 'a'
+count = 100000000000000000000000
+msg = "HI"
+
+print("number" + str(num))
+print("salary" + str(salary))
+print("var" + str(var))
+print("count" + str(count))
+print("msg" + str(msg))
